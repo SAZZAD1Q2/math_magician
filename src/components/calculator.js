769be5e62dd1f56b698f788dca99efd0a8d1005c
SAZZAD1Q2 <kurulia115@gmail.com>
2023-07-06 12:12:34 +0600

    logic files added

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
 import Screen from './screen';
 import buttonData from './buttonData.json';
+import calculate from '../logic/calculate';
 import './calculator.css';
 
 const Calculator = () => {
-  const [display] = useState('');
+  const [display, setDisplay] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({});
+
+  const handleClick = (buttonContent) => {
+    const newData = calculate(calculatorData, buttonContent);
+    setCalculatorData(newData);
+
+    if (newData.total !== null) {
+      setDisplay(newData.total);
+    } else if (newData.next !== null) {
+      setDisplay(newData.next);
+    } else {
+      setDisplay('0');
+    }
+  };
 
   const renderButtons = () => buttonData.map((button) => (
-    <button className={button.className} key={button.id} type="button">
+    <button
+      className={button.className}
+      key={button.id}
+      type="button"
+      onClick={() => handleClick(button.content)}
+    >
       {button.content}
     </button>
   ));
